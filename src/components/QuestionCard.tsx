31beecc8b4e1fb295f65dd420a043a9f5b77c17e
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-08-08 11:25:34 +0500

    update Question Card components

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { QuestionProps } from "../Types/QuizType";
+import { Wrapper, ButtonWrapper } from "./QuestionCard.style";
 
 const QuestionCard: React.FC<QuestionProps> = ({
   question,
@@ -10,25 +11,33 @@ const QuestionCard: React.FC<QuestionProps> = ({
   totalQuestions,
 }) => {
   return (
-    <div className="question-container">
-      <p className="number">
-        Question: {questionNumber}/ {totalQuestions}
-      </p>
-      <p dangerouslySetInnerHTML={{ __html: question }}></p>
-      <div>
-        {answers.map((answer, ind) => (
-          <div key={ind}>
-            <button
-              disabled={userAnswer ? true : false}
-              value={answer}
-              onClick={callback}
+    <Wrapper>
+      <div className="question-container">
+        <div className="quest">
+          <p className="number">
+            Question: {questionNumber}/ {totalQuestions}
+          </p>
+          <p dangerouslySetInnerHTML={{ __html: question }}></p>
+        </div>
+        <div>
+          {answers.map((answer, ind) => (
+            <ButtonWrapper
+              key={ind}
+              correct={userAnswer?.correctAnswer === answer}
+              userClicked={userAnswer?.answer === answer}
             >
-              <span dangerouslySetInnerHTML={{ __html: answer }}></span>
-            </button>
-          </div>
-        ))}
+              <button
+                disabled={userAnswer ? true : false}
+                value={answer}
+                onClick={callback}
+              >
+                <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+              </button>
+            </ButtonWrapper>
+          ))}
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 export default QuestionCard;
